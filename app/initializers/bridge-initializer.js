e4626ba86304886bb4abe41756f7a38d4f30ec93
pliljegr <pliljegr@linkedin.com>
2015-03-17 18:55:07 -0700

    Updating bad import and making assertion more accurate

--- app/initializers/bridge-initializer.js
+++ app/initializers/bridge-initializer.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import config from './config/environment';
+import config from '../config/environment';
 
 import ChromeExtensionBridge from 'ember-cli-inspector-bridge/ember-inspector-bridge';
 
@@ -7,7 +7,7 @@ export default {
   name: 'chrome-extension-bridge',
   initialize: function() {
     if(config.environment === 'development') {
-      Ember.assert('You must specify the inspectorID (from chrome://extensions) for the Ember Inspector', config.inspectorID);
+      Ember.assert('You must specify the inspectorID (from chrome://extensions) for the Ember Inspector', (typeof config.inspectorID === 'string'));
       return new ChromeExtensionBridge(config.inspectorID);
     }
   }
