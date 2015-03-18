fd5866ccd7727fb558a270a91c0c17e5d5ded7c7
pliljegr <pliljegr@linkedin.com>
2015-03-17 18:49:16 -0700

    Readding initializer after bad merge

---
+++ app/initializers/bridge-initializer.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+import ChromeExtensionBridge from 'ember-cli-inspector-bridge/ember-inspector-bridge';
+
+export default {
+  name: 'chrome-extension-bridge',
+  initialize: function() {
+    if(config.environment === 'development') {
+      Ember.assert('You must specify the inspectorID (from chrome://extensions) for the Ember Inspector', config.inspectorID);
+      return new ChromeExtensionBridge(config.inspectorID);
+    }
+  }
+};
